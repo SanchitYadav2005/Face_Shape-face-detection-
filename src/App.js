608562d0e14996193e2d2bb7dd0a696b43c24d02
unknown <sanchit2005yudavanshi@gmail.com>
2024-06-13 18:17:47 +0530

    added webcam component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-function App(){
- return(
-  <Navbar/>
- )
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
 }
 
-export default App
+export default App;
