3d0c8bb872be68a68b703e333cd4983fbf186a7b
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-06-17 16:30:44 +0530

    wokring on detecting the face

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import "./App.css";
 import Webcam from "react-webcam";
+import * as faceDetection from "@tensorflow-models/face-detection";
+import "@tensorflow/tfjs-backend-webgl";
 
 const App = () => {
-  const webcam = useRef(null);
+  const webcamRef = useRef(null);
+
+  const handleDetection = async () => {
+    const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
+    const detectorConfig = {
+      runtime: "mediapipe",
+    };
+
+    const detector = await faceDetection.createDetector(model, detectorConfig);
+    console.log(detector);
+
+    const detectFaces = async () => {
+      if (webcamRef.current && webcamRef.current.video.readyState === 4) {
+        const video = webcamRef.current.video;
+        const faces = await detector.estimateFaces(video);
+        console.log(faces);
+      }
+    };
+
+    setInterval(detectFaces, 1000); // Detect faces every second
+  };
+
+  useEffect(() => {
+    handleDetection();
+  }, []);
 
   return (
     <div className="App">
       <header className="header">
-        <div className="title">face mask App</div>
+        <div className="title">Face Shape</div>
       </header>
       <Webcam
         audio={false}
-        ref={webcam}
+        ref={webcamRef}
         style={{
           position: "absolute",
           margin: "auto",
@@ -24,6 +50,6 @@ const App = () => {
       />
     </div>
   );
-}
+};
 
 export default App;
