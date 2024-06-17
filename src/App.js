d2553e29fec6ad143477d817baf6205a856c067f
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-06-17 16:17:31 +0530

    wokring on tenserflow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
+import { useRef } from "react";
+import "./App.css";
+import Webcam from "react-webcam";
 
-function App() {
+const App = () => {
+  const webcam = useRef(null);
+
   return (
-    <>
-      <Navbar />
-      <Main />
-    </>
+    <div className="App">
+      <header className="header">
+        <div className="title">face mask App</div>
+      </header>
+      <Webcam
+        audio={false}
+        ref={webcam}
+        style={{
+          position: "absolute",
+          margin: "auto",
+          textAlign: "center",
+          top: 100,
+          left: 0,
+          right: 0,
+        }}
+      />
+    </div>
   );
 }
 
